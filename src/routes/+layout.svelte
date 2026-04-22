4f13707b6559f6e970a2c42601be39b49ac024a8
dai-rewahandi <niktodaire@gmail.com>
2026-04-22 11:58:32 -0700

    fix: update favicon path in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <svelte:head>
     <title>sosial_link</title>
     <meta name="description" content="A social media platform built with SvelteKit and Supabase" />
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="src/lib/assets/favicon.ico" type="image/x-icon" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" >
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
